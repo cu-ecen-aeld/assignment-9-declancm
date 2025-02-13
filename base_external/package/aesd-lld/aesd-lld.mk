df035120ecce33eb5388f46e81435fa5a9476222
Declan Mullen <declanmullen3.14@gmail.com>
2025-02-13 03:00:29 -0800

    Implemented LLD package

---
+++ base_external/package/aesd-lld/aesd-lld.mk
@@ -0,0 +1,16 @@
+
+##############################################################
+#
+# AESD-LLD
+#
+##############################################################
+
+AESD_LLD_VERSION = '93ba357555f43d64d5e6ec027c924d2ebbb9adc7'
+AESD_LLD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-declancm.git'
+AESD_LLD_SITE_METHOD = git
+AESD_LLD_GIT_SUBMODULES = YES
+
+AESD_LLD_MODULE_SUBDIRS = misc-modules scull
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
